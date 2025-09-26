2e28785b4ec63704b2a84fb1c4a1f3dcca7c5f07
aman035 <guptaaman200115@gmail.com>
2025-09-27 00:21:53 +0530

    add: agent module

--- backend/src/nft/dto/get-nfts-by-address.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class GetNftsByAddressDto {
-  @IsString()
-  address!: string;
-
-  @IsString()
-  chain!: string; // e.g., 'ethereum' | 'base' | 'polygon'
-}
